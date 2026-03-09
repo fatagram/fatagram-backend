36e3ffd2552bdbe2bd6ff3dfb3c42f4865a8eaa6
ngocfat <ngocphatc2710@gmail.com>
2026-03-09 11:47:42 +0700

    refactor: refactor conf

--- Fatagram.API/Extensions/Configuration/ConfigurationExtensions.cs
+++ Fatagram.API/Extensions/Configuration/ConfigurationExtensions.cs
@@ -9,13 +9,14 @@ namespace Fatagram.API.Extensions.Configuration
     {
         public static void AddAppConfiguration(this WebApplicationBuilder builder)
         {
+            var env = builder.Environment.EnvironmentName;
+
             builder
-                .Configuration.AddJsonFile(
-                    "appsettings.json",
-                    optional: false,
-                    reloadOnChange: true
-                )
-                .AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
+                .Configuration.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.Local.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
         }
     }
 }

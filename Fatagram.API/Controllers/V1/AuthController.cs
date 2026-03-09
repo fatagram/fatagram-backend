76ecf00e01c20cf0d12f141926e6107351af2862
ngocfat <ngocphatc2710@gmail.com>
2026-03-09 23:10:37 +0700

    make some error to test CI

--- Fatagram.API/Controllers/V1/AuthController.cs
+++ Fatagram.API/Controllers/V1/AuthController.cs
@@ -56,7 +56,7 @@ namespace Fatagram.API.Controllers.V1
             [FromBody] OAuthCallbackDto request
         )
         {
-            _logger.LogInformation(
+            int x = _logger.LogInformation(
                 "Received OAuth callback for provider: {Provider} with code: {Code}",
                 provider,
                 request.Code

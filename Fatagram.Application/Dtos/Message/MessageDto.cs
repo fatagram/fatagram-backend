3bdbd2589329b5bc5a5e9161872e981c6859dd97
ngocfat <ngocphatc2710@gmail.com>
2026-03-24 02:54:10 +0700

    feat: add client temp id to message request

--- Fatagram.Application/Dtos/Message/MessageDto.cs
+++ Fatagram.Application/Dtos/Message/MessageDto.cs
@@ -11,18 +11,22 @@ namespace Fatagram.Application.Dtos.Message
         public Guid? ConversationId { get; set; }
         public Guid? ReceiverId { get; set; }
         public Guid? CorrelationId { get; set; }
+        public Guid? ClientTempId { get; set; }
         public string Content { get; set; } = string.Empty;
     }
 
     public class ResponseMessageDto
     {
         public Guid Id { get; set; }
-
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public Guid? ConversationId { get; set; }
         public bool IsGroup { get; set; }
         public Guid SenderId { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public Guid? CorrelationId { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Guid? ClientTempId { get; set; }
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
     }

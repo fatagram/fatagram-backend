6267a6381e4a0b1b929cb7bd5838256947ff1665
ngocfat <ngocphatc2710@gmail.com>
2026-03-30 19:48:11 +0700

    fix

--- Fatagram.Application/Services/ImageService/ImageSharpAdapter.cs
+++ Fatagram.Application/Services/ImageService/ImageSharpAdapter.cs
@@ -15,19 +15,15 @@ namespace Fatagram.Application.Services.ImageService
             stream.Position = 0;
             var image = await Image.LoadAsync(stream);
 
-            // Only resize when both width and height are provided (> 0).
-            if (spec.Width > 0 && spec.Height > 0)
-            {
-                image.Mutate(x =>
-                    x.Resize(
-                        new ResizeOptions
-                        {
-                            Mode = MapResizeMode(spec.ResizeMode),
-                            Size = new Size(spec.Width, spec.Height),
-                        }
-                    )
-                );
-            }
+            image.Mutate(x =>
+                x.Resize(
+                    new ResizeOptions
+                    {
+                        Mode = MapResizeMode(spec.ResizeMode),
+                        Size = new Size(spec.Width, spec.Height),
+                    }
+                )
+            );
 
             return image;
         }
